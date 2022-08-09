f349a744132d4aedb07213f803064e751af061aa
Yuhee Kim <dbgml4509@naver.com>
2022-08-09 21:25:06 +0900

    fix : kotlin reflection 오류 수정

--- app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 const val API_KEY = "API"
