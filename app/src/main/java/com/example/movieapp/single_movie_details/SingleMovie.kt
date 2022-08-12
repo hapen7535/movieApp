b0968c367c15b72fb83204666186260592e118c3
Yuhee Kim <dbgml4509@naver.com>
2022-08-12 21:25:10 +0900

    feat : view에 데이터 받아오기

--- app/src/main/java/com/example/movieapp/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/movieapp/single_movie_details/SingleMovie.kt
@@ -1,12 +1,41 @@
 package com.example.movieapp.single_movie_details
 
+import android.graphics.Movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.movieapp.R
+import com.example.movieapp.data.api.TheMovieDBClient
+import com.example.movieapp.data.api.TheMovieDBInterface
 
 class SingleMovie : AppCompatActivity() {
+
+    private lateinit var viewModel: SingleMovieViewModel
+    private lateinit var movieRepository : MovieDetailsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_movie)
+
+        val movieId : Int = 1
+        val apiService : TheMovieDBInterface = TheMovieDBClient.getClient()
+        movieRepository = MovieDetailsRepository(apiService)
+
+        viewModel = getViewModel(movieId)
+        viewModel.movieDetails.observe(this, Observer {
+
+        })
     }
+
+    private fun getViewModel(movieId : Int) : SingleMovieViewModel{
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass : Class<T>) : T {
+                @Suppress("UNCHECKED_CAST")
+                return SingleMovieViewModel(movieRepository, movieId) as T
+            }
+        })[SingleMovieViewModel::class.java]
+    }
+
 }
